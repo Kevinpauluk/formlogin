ff690820ade1453cac071c0d9849faa6e3f35832
K3vin1v4 <kevin.pauluk@escola.pr.gov.br>
2025-06-23 08:38:47 -0300

    script

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 document.getElementById("loginform").addEventListener("submit", function (e) {
-    
+    e.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const errormenssage = document.getElementById ("error-menssage");
+
+
+    const validusername = "admin";
+    const validpassword = "admin";
+
+    if (username) {
+        
+    } else {
+        
+    }
 });
